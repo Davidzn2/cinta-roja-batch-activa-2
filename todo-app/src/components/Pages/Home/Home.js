3bde22cc8e33a16411d5ad9d8a867762c084658d
davidz <davidzn2@gmail.com>
2020-07-07 18:00:19 -0500

    Get all todos

--- todo-app/src/components/Pages/Home/Home.js
+++ todo-app/src/components/Pages/Home/Home.js
@@ -1,33 +1,62 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../layout/Header/Header";
 import Footer from "../../layout/Footer/Footer";
-
-import axios from 'axios';
+import TodoCard from "../../TodoCard/TodoCard";
+import axios from "axios";
 function Home() {
   const [todos, setTodos] = useState([]);
   const URL = "https://todos-back-devf-activa.herokuapp.com/todos/";
-  const getTodos = ()=>{
+  useEffect(() => {
     axios.get(URL)
-      .then((response)=>{
-        let allTodos = response.data
-        setTodos(allTodos)
-      }).catch((err)=>{
-        console.log(err)
+      .then((response) => {
+        let allTodos = response.data;
+        setTodos(allTodos);
       })
-  }
-  getTodos()
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-        <h2>Bienvenido, esto es lo que hay por hacer:</h2>
-        <ul>
-        {
-          todos.map((todo)=>{
-            return <li key={todo._id}>{todo.name}</li>
-          })
-        }
-        </ul>
-       
+      <h2>Bienvenido, esto es lo que hay por hacer:</h2>
+      <div className="container">
+        <h3>Alta</h3>
+        {todos.map((todo) => {
+          return todo.priority === 1 ? 
+            <TodoCard
+              priority={todo.priority}
+              name={todo.name}
+              done={todo.done}
+              key={todo._id}
+            />
+           : undefined;
+        })}
+        <h3>Media</h3>
+        {todos.map((todo) => {
+          return todo.priority === 2 ? 
+            <TodoCard
+              priority={todo.priority}
+              name={todo.name}
+              done={todo.done}
+              key={todo._id}
+            />
+           : undefined;
+        })}
+        <h3>Baja</h3>
+        {todos.map((todo) => {
+          return todo.priority === 3 ? 
+            <TodoCard
+              priority={todo.priority}
+              name={todo.name}
+              done={todo.done}
+              key={todo._id}
+            />
+           : undefined;
+        })}
+      </div>
+
       <Footer />
     </>
   );
